8242272d6645163d278d8bf4872ee1e6ea5b4ec1
Gopalakannan55 <2021062058@selvamtech.edu.in>
2025-07-11 11:59:14 +0530

    recap python basic

---
+++ Python/Basic.py
@@ -0,0 +1,16 @@
+print("Hello, World!")
+
+# Variables and Data Types
+a = 10        
+b = 5.5       
+name = "Ram"  
+flag = True   
+
+# Input and Output
+name = input("Enter your name: ")
+age=int(input("Enter your age:"))
+print("Hello", name,"age:",age)
+
+# Type Conversion
+x = int("10")     # str to int 
+y = str(100)      #int to str
